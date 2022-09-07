07c9e91d54bedcec262083237a9488d0ce01ab63
321dhruuv0037 <321dhruuv0037@dbit.in>
2022-09-07 13:32:32 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml")); //pass scene name here
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+            stage.setResizable(false);
+            stage.setTitle("CREST : Personal Finance Protfolio Manager");
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToSignUp(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("sign-up.fxml")); //pass scene name here
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToHome(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("hello-view.fxml")); //pass scene name here
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    public void switchToMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("menu.fxml")); //pass scene name here
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
